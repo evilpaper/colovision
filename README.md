f1b02e1e2d4189a11901b4df100adfe8e907d401
Pelle Lundgren <pellburt@gmail.com>
2025-08-07 22:48:21 +0200

    feat: initial commit, setting up project

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a small proof of concept of sprite based animation with HTML, CSS and JS.
+
+It show a Sonic running animation.
---
+++ sonic.js
@@ -0,0 +1,40 @@
+class Sonic extends Sprite {
+  static src = "sonic-spritesheet.png";
+
+  constructor(x, y, context, image) {
+    super({
+      context: context,
+      image: image,
+      x: x,
+      y: y,
+      width: 114,
+      height: 120,
+      frameIndex: 0,
+      row: 0, // Default to idle animation
+      tickCount: 0,
+      ticksPerFrame: 4, // This should probably be overridden by the animation type
+      frames: 8,
+    });
+  }
+
+  walk() {
+    this.frames = 8;
+    this.frameIndex = 0;
+    this.row = 1;
+    this.ticksPerFrame = 4;
+  }
+
+  run() {
+    this.frames = 4;
+    this.frameIndex = 0;
+    this.row = 2;
+    this.ticksPerFrame = 2;
+  }
+
+  idle() {
+    this.frames = 9;
+    this.frameIndex = 0;
+    this.row = 0;
+    this.ticksPerFrame = 12;
+  }
+}
